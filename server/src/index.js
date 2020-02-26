45c83b77530174d3a8a5c0af7f7d52d837ccd447
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-02-26 18:49:23 +0100

    Refactor

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,18 @@ const router = require("./router")
 const postgres = require("./postgres")
 const session = require("./middleware/session")
 const socket = require("./socket")
+const https = require("https")
+const http = require("http")
+const fs = require("fs")
+
+function getOptions() {
+  try {
+    return {
+      key: fs.readFileSync(process.env.KEY),
+      cert: fs.readFileSync(process.env.CERT),
+    }
+  } catch  { }
+}
 
 function initialize() {
   postgres.init().then(run).catch(() => setTimeout(initialize, 1000))
@@ -10,13 +22,17 @@ function initialize() {
 
 function run() {
   const app = express()
+  const options = getOptions()
 
   app.use(session)
   app.use(express.json())
   app.use(router)
 
-  const server = app.listen(3000, () => console.log("Server started"))
-  socket(server)
+  if (options) {
+    socket(https.createServer(options, app).listen(3443, () => console.log("HTTPS server started")))
+  } else {
+    socket(http.createServer(app).listen(3080, () => console.log("HTTP server started")))
+  }
 }
 
 initialize()
